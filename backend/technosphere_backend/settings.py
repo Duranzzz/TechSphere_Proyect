71781f6394f8ae5ec6a254b2bd0393e99195e147
Duranzzz <leonardodurancuenca@gmail.com>
2025-11-16 19:40:33 -0400

    Fixed: Problema de reconocimiento de importacion relativa por parte de Django

--- backend/technosphere_backend/settings.py
+++ backend/technosphere_backend/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Variable de entorno que importa la contraseña de Mysql
-from ..config_local import MYSQL_PASSWORD
+from .config_local import MYSQL_PASSWORD
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
